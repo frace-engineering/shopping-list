7544c1e3c8a0fac1272786b52e47967d370f2e23
Ugwu Friday <fugwungene@gmail.com>
2022-07-24 08:50:54 +0100

    worked on the responsiveness

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const addList = document.getElementById('btn');
 const item = document.getElementById('inputitem');
-const shoppingList = document.getElementById('list');
+const shoppingList = document.querySelector('.listitems');
+// const quantity = document.querySelector('.quantity');
+// const unitPrice = document.querySelector('.unit-price');
+// const total = document.querySelector('.total');
+// const grandTotal = document.querySelector('.grand-total');
 
 
 addList.addEventListener('click', ()=>{
     const orderedList = document.createElement('li');
+
     orderedList.classList.add('list-style');
     orderedList.innerText=item.value;
     if(item.value!=""){
-    shoppingList.appendChild(orderedList);}
+    shoppingList.appendChild(orderedList);
+    // unitPrice.appendChild();
+    // total.appendChild();
+    // grandTotal.appendChild();
+}
     item.value="";
     console.log(item);
-    orderedList.addEventListener("dbclick",()=>{
-        shoppingList.removedChild(orderedList);
-    })
+    // orderedList.addEventListener("dbclick",()=>{
+    //     shoppingList.removedChild(orderedList);
+    // })
 })
